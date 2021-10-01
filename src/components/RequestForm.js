cfe65e080d8fb64f678b2db2f7af283b647e68dc
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2021-09-30 21:15:22 -0700

    Fixed build error

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -5,25 +5,6 @@ import { useEffect, useState } from 'react';
 import { GetNFTOwnedByUser, GetNFTCollection, contract } from "../utils";
 import { getCurrentWalletConnected, connectWallet } from "../utils/walletHelper.js";
 
-const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-    { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-]
-
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
