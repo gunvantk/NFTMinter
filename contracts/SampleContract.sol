0f90b0345263491495faa6f96fee16c1dfb393c9
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2021-10-05 00:22:54 -0700

    Working contract

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -1,16 +1,15 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SampleContract is ERC721 {
+contract SampleContract is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor(string memory tokenName, string memory symbol) ERC721(tokenName, symbol) {
-        _setBaseURI("ipfs://");
+    constructor() ERC721("Beatmap", "BMap") {        
     }
 
     function mintToken(address owner, string memory metadataURI)
--- src/components/MintForm.js
+++ src/components/MintForm.js
@@ -122,7 +122,7 @@ function MinterForm() {
               type="button"
               onClick={handleMintNFT}
               disabled={!mintEnabled} 
-              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
             >
               Mint
             </button>
